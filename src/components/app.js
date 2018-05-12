9af94ff56bd5527ff84f626b83c810a48a16b283
dundaswest <hello@eunbyulcho.me>
2018-05-12 10:53:57 -0700

    YouTube data now correctly populates the videoList

--- src/components/app.js
+++ src/components/app.js
@@ -2,19 +2,23 @@ angular.module('video-player')
   .component('app', {
     templateUrl: 'src/templates/app.html',
     controller: function(youTube) {
-      
+
       this.videos = null;
       // this.videos = exampleVideoData;
 
       this.currentVideo = this.videos ? this.videos[0] : null;
       
-      this.searchResults = function(response) {
-        console.log('Search Response', response.data.items);
-        this.videos = response.data.items;
-      };
+      this.$onInit = function() {
+        youTube.getVideos('David Bowie', this.searchResults);
+      };      
 
       this.selectVideo = function() {
       };
+
+      this.searchResults = function(response) {
+        this.videos = response.data.items;
+        this.currentVideo = this.videos[0];
+      }.bind(this);
 
       this.youTubeData = function(query, callback) {
         query = 'David Bowie';
--- src/components/search.js
+++ src/components/search.js
@@ -2,6 +2,6 @@ angular.module('video-player')
   .component('search', {
     templateUrl: 'src/templates/search.html',
     bindings: {
-      search:'<'
+      search: '<'
     }
   });
