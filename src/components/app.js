91815706e4e8ac6a44d215970a0445dc3f5770f5
dundaswest <hello@eunbyulcho.me>
2018-05-11 21:11:01 -0700

    Wrapping up for the night

--- src/components/app.js
+++ src/components/app.js
@@ -3,24 +3,26 @@ angular.module('video-player')
     templateUrl: 'src/templates/app.html',
     controller: function(youTube) {
       
-
       this.youTubeData = youTube.getVideos('David Bowie');
 
-      this.videos = exampleVideoData;
-
       // this.videos = this.youTubeData.data.items;
+      this.videos = exampleVideoData;
 
       this.currentVideo = this.videos[0];
       
       this.searchResults = function() {
         return;
       };
+
       this.selectVideo = function() {
         return;
       };
+
       this.onClick = function(video) {
         this.currentVideo = video;        
       }.bind(this);
 
     },
   });
+  
+// ng-if="$ctrl.videos"
